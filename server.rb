e9ac57da85fbf2115a91c25c93a4dc6c63c41828
nicolasjesse <nicolasjesseam@gmail.com>
2022-07-18 02:56:06 -0300

    fix outdated method calls

--- server.rb
+++ server.rb
@@ -2,9 +2,10 @@ require 'sinatra'
 require 'rack/handler/puma'
 require 'csv'
 require_relative './infra/examination_repo.rb'
+require_relative './infra/database_core.rb'
 
 get '/tests' do
-  exam_repo = ExaminationRepo.new
+  exam_repo = ExaminationRepo.new(DatabaseCore.get_connection)
   exam_repo.get_all.values.to_json
 end
 
