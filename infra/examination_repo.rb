9e027eb99badad4a60996bbbb252524cfa0f2b06
nicolasjesse <nicolasjesseam@gmail.com>
2022-07-21 08:48:11 -0300

    add method get_by_token

--- infra/examination_repo.rb
+++ infra/examination_repo.rb
@@ -25,6 +25,33 @@ class ExaminationRepo
     end
   end
 
+  def get_by_token(token)
+    begin
+      result = @conn.exec("SELECT * FROM examination WHERE token='#{token}'").values
+      tests = result.map do |test|
+        { type: test[14],
+          limits: test[15],
+          result: test[16]
+        }
+      end
+      { result_token: token, 
+        result_date: result[0][13],
+        cpf: result[0][6],
+        name: result[0][7],
+        email: result[0][4],
+        birthday: result[0][9],
+        doctor: {
+          crm: result[0][2],
+          crm_state: result[0][5],
+          name: result[0][3]
+        },
+        tests: tests
+      }
+    rescue => exception
+      raise exception
+    end
+  end
+
   def get_all
     begin
       result = @conn.exec("SELECT * FROM examination;")
